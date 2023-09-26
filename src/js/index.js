129a1d90dfc3809cbdeb8adbfc0b10d9fd257808
Thiagomagalhaeslucio <131904877+Thiagomagalhaeslucio@users.noreply.github.com>
2023-09-26 13:52:39 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+function togglemode() {
+  const html = document.body
+  html.classList.toggle("light")
+
+  //if(body.classList.contains('light')) {
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./src/assets/Avatar-escuro.png")
+  } else {
+    img.setAttribute("src", "src/assets/avatar-claro.png")
+  }
+}
